4f2ec1b6edcc86b749dfaea569b7c4548732ea43
Lukas Dür <lukas.dduer@gmail.com>
2022-11-09 20:37:19 +0100

    Definition der Variablen

--- Projekt Applikation/Scripts/Astronaut.gd
+++ Projekt Applikation/Scripts/Astronaut.gd
@@ -1,11 +1,13 @@
 extends KinematicBody2D
 
+var gravity = 0 #Abhängig vom Planet 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+export var jump_force = 1 #Kann auch geändert werden, je nach Austronautentyp (Mensch, 930 N,  Känguru, ....)
+export var mass = 75 #Kann im UI eingestellt werden
+
+var velocity = 0
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	$AnimatedSprite.speed_scale = 3
+
