b365e9db5883ca58f6f0ca6080cbb12bd9fea682
Lukas Dür <lukas.dduer@gmail.com>
2022-11-14 11:46:08 +0100

    Astronaut Jump

--- Projekt Applikation/Scripts/Astronaut.gd
+++ Projekt Applikation/Scripts/Astronaut.gd
@@ -1,13 +1,30 @@
 extends KinematicBody2D
 
-var gravity = 0 #Abhängig vom Planet 
+var screen_size
+
+var gravity = 2 #Abhängig vom Planet 
 
 export var jump_force = 1 #Kann auch geändert werden, je nach Austronautentyp (Mensch, 930 N,  Känguru, ....)
 export var mass = 75 #Kann im UI eingestellt werden
 
-var velocity = 0
+export var speed = 100
 
 
 func _ready():
-	$AnimatedSprite.speed_scale = 3
-
+	screen_size = get_viewport_rect().size
+	
+func _process(delta):
+	var velocity = Vector2.ZERO
+	if Input.is_action_just_pressed("jump"):
+		velocity.y -= 1  #oder Formel für die Gravitation 
+		velocity.y += gravity
+		
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite.play()
+	#else:
+		#$AnimatedSprite.stop()
+		
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
