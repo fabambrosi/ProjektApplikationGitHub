6471473c33a88f8deb7d6faed778befdf7d59a13
fabambrosi <114085904+fabambrosi@users.noreply.github.com>
2022-12-16 09:52:53 +0100

    Signal Bug Test

--- Projekt Applikation/Scripts/Astronaut.gd
+++ Projekt Applikation/Scripts/Astronaut.gd
@@ -22,7 +22,6 @@ func _process(delta):
 	
 	gravity = get_parent().get_node("Planet").planet_gravity
 	velocity.x = 0
-	print(gravity)
 	velocity.y += gravity * delta * motion_factor
 	check_key_input()
 
