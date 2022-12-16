fd32e2f5f2ebd948e485e2da1512d7703e82ff79
Lukas Dür <lukas.dduer@gmail.com>
2022-12-16 12:02:53 +0100

    Erweiterung der Steuerung und Änderung des UI

--- Projekt Applikation/Scenes/UserInterface.gd
+++ Projekt Applikation/Scenes/UserInterface.gd
@@ -3,6 +3,7 @@ extends Control
 onready var planet_node = get_parent().get_parent().get_node("Planet")
 onready var planet_selector = $PanelContainer/HBoxContainer/PlanetVBox/OptionButton
 onready var gravity_text = $PanelContainer/HBoxContainer/PlanetVBox/LineEdit
+onready var gravity_slider = $PanelContainer/HBoxContainer/PlanetVBox/VSlider
 
 var planet_list = ["Sonne", "Merkur", "Venus", "Erde", "Mars", "Jupiter", "Saturn", "Uranus", "Neptun", "Pluto", "Mond"]
 var gravity_dictionary = {"Sonne": 274, "Merkur":3.7,"Venus":8.87,"Erde":9.81, "Mars":3.69, "Jupiter":24.79, "Saturn":10.44,"Uranus":8.87,"Neptun":11.15,"Pluto":0.66,"Mond":1.63}
@@ -15,7 +16,7 @@ func _ready():
 	update_planet()
 
 func _physics_process(_delta):
-	planet_node.planet_gravity = float(gravity_text.text)
+	planet_node.planet_gravity = gravity_text.value
 
 func _on_OptionButton_item_selected(_index):
 	update_planet()
@@ -23,4 +24,10 @@ func _on_OptionButton_item_selected(_index):
 func update_planet():
 	current_planet = planet_list[planet_selector.selected]
 	planet_node.planet = current_planet
-	gravity_text.text = str(gravity_dictionary[current_planet])
+	gravity_text.value = gravity_dictionary[current_planet]
+
+func _on_VSlider_drag_ended(value_changed):
+	gravity_text.value = gravity_slider.value
+
+func _on_LineEdit_value_changed(value):
+	gravity_slider.value = value
--- Projekt Applikation/Scripts/Astronaut.gd
+++ Projekt Applikation/Scripts/Astronaut.gd
@@ -8,32 +8,36 @@ var motion_factor = 30
 export var jump_force = 160 #Kann auch geändert werden, je nach Austronautentyp (Mensch, 930 N,  Känguru, ....)
 export var mass = 75 #Kann im UI eingestellt werden
 
-
 var velocity = Vector2()
 const Up_Vector = Vector2(0, -1)
 
-var z = 0
-
 func _ready():
-	
 	screen_size = get_viewport_rect().size
 
 func _process(delta):
-	
 	gravity = get_parent().get_node("Planet").planet_gravity
 	velocity.x = 0
 	velocity.y += gravity * delta * motion_factor
 	check_key_input()
+	move()
+	reset_position()
 
-	
-# warning-ignore:return_value_discarded
-	move_and_slide(velocity, Up_Vector)
-	#position += velocity * delta
-	#position.x = clamp(position.x, 0, screen_size.x)
-	#position.y = clamp(position.y, 0, screen_size.y)
-	
-	
+	velocity = move_and_slide(velocity, Up_Vector)
 
 func check_key_input():
 	if Input.is_action_just_pressed("w") and is_on_floor():
 		velocity.y = -jump_force  
+
+func move():
+	if Input.is_action_pressed("a"):
+		position.x = position.x -1
+	if Input.is_action_pressed("d"):
+		position.x = position.x +1
+
+func reset_position():
+	if position.y > screen_size.y:
+		position.y = -10
+	if position.x < -10:
+		position.x = screen_size.x + 8
+	if position.x > screen_size.x + 10:
+		position.x = - 8
